9e2101a4c9bf1f7b671dbf7297acdd8af828a131
minsoo822@naver.com <minsoo822@naver.com>
2022-07-28 17:31:01 +0900

    aaa

---
+++ src/com/baewooga/study/ch02/ByteExample.java
@@ -0,0 +1,30 @@
+package com.baewooga.study.ch02;
+
+public class ByteExample {
+
+	public static void main(String[] args) {
+		
+		byte var1 = -128;
+		byte var2 = -30;
+		byte var3 = 0;
+		byte var4 = 30;
+		byte var5 = 127;
+//		byte var6 = -128;
+		
+		
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(var5);
+//		System.out.println(var6);  //에러 .  byte는 -128 ~ 127 까지.
+//								   byte < short < int <long
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/com/baewooga/study/ch02/Hello.java
@@ -0,0 +1,47 @@
+package com.baewooga.study.ch02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+//		데이터타입 명수명 = 값;
+//		데이터타입 변수명
+		
+		boolean abc = true;
+		boolean xyz;
+		xyz = false;
+		
+		int width = 100;
+		String name = "민수";
+		
+		//--------------------------------------------------
+		
+		int dab = 19920622;						//생년월일
+		int height = 175; 						//키
+		int weight = 55; 						//몸무게
+		String email = "minsoo822@naver.com";	//이메일
+		int phone = 01045000161; 				//전화번호
+		String hobby = "사투리";					//특기
+		String eye = "겉쌍꺼풀";					//쌍꺼풀
+		String voice = "중음";					//보이스톤
+		String insta = "mins._oo";				//인스타
+		String facebook = "alstnWKd";			//페이스북
+	
+		
+		System.out.println(name);
+		System.out.println("name 변수에 들어가 있는 값은: " + name);
+		
+		System.out.println(dab);
+		System.out.println("dab 변수에 들어가 있는 값은 : " + dab);
+		
+		System.out.println(height);
+		System.out.println("height 변수에 들어가 있는 값은 : " + height);
+		
+		System.out.println(weight);
+		System.out.println("weight 변수에 들어가 있는 값은 : " + weight);
+		
+		System.out.println(abc);
+		
+	}
+
+}
