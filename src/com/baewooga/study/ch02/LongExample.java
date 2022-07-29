2bef02fae048d5254ae25204c62ecd099d1d43fc
minsoo822@naver.com <minsoo822@naver.com>
2022-07-29 15:55:56 +0900

    java

---
+++ src/com/baewooga/study/ch02/LongExample.java
@@ -0,0 +1,18 @@
+package com.baewooga.study.ch02;
+
+public class LongExample {
+
+	public static void main(String[] args) {
+		 
+		long var1 = 10;
+		long var2 = 2045654644;			//40456546443은 에러 이때 L 을써주면 ok
+		
+//		long var3 = 10000000000000;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+//		System.out.println(var3);
+
+	}
+
+}
