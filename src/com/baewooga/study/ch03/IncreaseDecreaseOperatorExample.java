a74102f1ef36b7f3c421bab08afddd97e32f8a75
minsoo822@naver.com <minsoo822@naver.com>
2022-08-02 15:51:28 +0900

    java

---
+++ src/com/baewooga/study/ch03/IncreaseDecreaseOperatorExample.java
@@ -0,0 +1,58 @@
+package com.baewooga.study.ch03;
+
+public class IncreaseDecreaseOperatorExample {
+
+	public static void main(String[] args) {
+
+//		++/-- 피연산자 : 다른 연산 수행 전에 피연산자 1증가/감소
+//		피연산자 ++/-- : 다른 연산 수행 후에 피연산자 1증가/감소
+
+		int i = 0;
+		int j = 0;
+		
+		++i;
+		j++;
+		
+		System.out.println("i: " +i);
+		System.out.println("j: " +j);
+		
+//		int 변수 a,b 를 선언하고 초기값으로 1을 넣어준다
+//		a 앞에 감소연산
+//		b 뒤에 감소연산
+//		두 변수의 결과를 출력
+		
+		int a = 1;
+		int b = 1;
+		
+		--a;
+		b--;
+		
+		System.out.println("a: " +a);
+		System.out.println("b: " +b);
+		
+//-------------------------------------------------------------
+		
+//		int 변수 k 선언후 초기값으로 i뒤에 증감연산
+		
+		int k = i++;
+		
+		System.out.println("k: " + k);
+		System.out.println("i: " + i);
+		
+		int m = ++j;
+		
+		System.out.println("m: " + m);
+		System.out.println("j: " + j);
+		
+		// i = 2
+		// j = 2
+		
+		int z = ++i + j++;
+
+		System.out.println("z: " + z);		// 5
+		System.out.println("i: " + i);		// 3
+		System.out.println("j: " + j);		// 3
+	
+	}
+
+}
--- src/com/baewooga/study/ch03/SignOperatorExample.java
+++ src/com/baewooga/study/ch03/SignOperatorExample.java
@@ -3,8 +3,24 @@ package com.baewooga.study.ch03;
 public class SignOperatorExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int x = 100;
+		
+		int result1 = +x;
+		int result2 = -x;
+		
+		System.out.println(result1);
+		System.out.println(result2);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
