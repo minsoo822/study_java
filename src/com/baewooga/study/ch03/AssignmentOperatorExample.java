c5734d01da84401ad8d6302327da695d0d6f3a81
minsoo822@naver.com <minsoo822@naver.com>
2022-08-05 11:22:40 +0900

    aaa

---
+++ src/com/baewooga/study/ch03/AssignmentOperatorExample.java
@@ -0,0 +1,35 @@
+package com.baewooga.study.ch03;
+
+public class AssignmentOperatorExample {
+
+	public static void main(String[] args) {
+		
+//		+=
+		
+		int result1 = 10;
+		
+		result1 += 10;
+//		result1 = result1 + 10;
+		System.out.println("result1: " + result1);
+		
+		
+//		-=
+		
+		int result2 = 10;
+		
+		result2 -= 10;
+//		result1 = result1 - 10;
+		System.out.println("result2: " + result2);
+		
+		
+//		*=
+		
+		int result3 = 10;
+		
+		result3 *= 10;
+//		result1 = result1 * 10;
+		System.out.println("result3: " + result3);
+		
+	}
+
+}
