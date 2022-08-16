1d6c1424377abef23bf7128118abaeb179e51fbd
minsoo822@naver.com <minsoo822@naver.com>
2022-08-16 17:36:01 +0900

    aa

---
+++ src/com/baewooga/study/ch04/ForExample.java
@@ -0,0 +1,107 @@
+package com.baewooga.study.ch04;
+
+public class ForExample {
+
+	public static void main(String[] args) {
+
+
+//		**********
+//		**********
+//		**********
+//		**********
+//		**********
+//		출력하는 프로그램을 만드시오
+		
+//		String i = "*";
+		
+//		for(int i=1; i<=10; i++) {
+//			if()
+//		}
+//		if() {
+//			System.out.println("**********");
+//		} else () {
+//			System.out.println("**********");
+//		}
+		
+		
+		
+		
+		for(int i=0; i<5; i++) {
+			
+			
+			for(int j = 1; j<=10; j++) {
+			
+			
+				System.out.print("*");
+			
+			
+			}
+			System.out.println();
+			
+			
+		}
+		
+		for(int i = 2; i<=9; i++) {
+			for(int j =1; j<=9; j++) {
+				System.out.println(i + " * " + j + " = " + i*j);
+			}
+		}
+		
+
+		
+		
+//		***********
+//		*         *
+//		*         *
+//		*         *
+//		***********
+//		출력하는 프로그램을 만드시오
+		
+//		if() {
+//			System.out.println("***********");
+//		} else {
+//			System.out.println("*         *");
+//		}
+		for(int i=1; i<=5; i++) {
+			if (i == 3) {
+				System.out.println("*         *");
+			} else if(i%2 != 0) {
+				System.out.println("***********");
+			} else {
+				System.out.println("*         *");
+			}
+		} 
+		
+		
+		System.out.println("----------------------");
+		
+		
+		
+//		1
+//		12
+//		123
+//		1234
+//		12345
+//		123456
+//		1234567
+//		12345678
+//		123456789
+//		1 3 5 7 90
+//		출력하는 프로그램을 만드시오
+		
+//		for(int i = 2; i < 10; i++) {
+//			for(int j=1; j<10; j++) {
+//				int sum = 1;
+//				System.out.println(sum );
+//			}
+//		}
+		
+		for(int i = 1; i <= 10; i++) {
+			for(int j = 1; j<=9; j++) {
+				
+			}
+		}
+		
+	}
+
+}
