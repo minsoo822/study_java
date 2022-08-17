4d4960aebfa10557252ca0717cc3f9be56fa3805
minsoo822@naver.com <minsoo822@naver.com>
2022-08-17 19:02:16 +0900

    java

---
+++ src/com/baewooga/study/ch04/ForExample2.java
@@ -0,0 +1,142 @@
+package com.baewooga.study.ch04;
+
+public class ForExample2 {
+
+	public static void main(String[] args) {
+		
+		
+//		**********
+//		**********
+//		**********
+//		**********
+//		**********
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.print("*");
+				} System.out.println();	
+			} 
+			
+//		0123456789
+//		0123456789
+//		0123456789
+//		0123456789
+//		0123456789
+		
+		for (int i = 0; i < 5; i++) {
+			
+			for (int j = 0; j < 10; j++) {
+				
+				System.out.print(j);
+				
+			} 
+			
+			System.out.println();
+		}
+		
+		
+//		*
+//		**
+//		***
+//		****
+//		*****
+		
+		
+		
+//		for (int i = 0; i <= 5; i++) {
+//			
+//			for (int j = 0; j < 5; j++) {
+//				System.out.println(i +"," +j);				
+//				
+//			}
+//			
+//		}	
+		
+		
+//		for (int i = 1; i <= 5; i++) {
+//				
+//			for (int j = 0; j < i; j++) {
+//				
+//				System.out.println(i +"," +j);				
+//					
+//			}
+//					
+//		}	
+		
+		
+//		*
+//		**
+//		***
+//		****
+//		*****
+		
+		for (int i = 1; i <= 5; i++) {
+			
+			for (int j = 1; j <= i; j++) {
+				
+				System.out.print("*");
+				
+			}
+			
+			System.out.println();
+			
+		}
+		
+		
+		
+//		구구단
+//		
+//		for (int i = 2; i < 10; i++) {
+//			for (int j = 1; j < 10; j++) {
+//				System.out.println(i + " * " + " = " + (i*j));
+//			} System.out.println("-----------------------");
+//		}
+//		
+//		
+//		1 ~ 100 까지의 숫자중 3 과 5 의 배수를 출력하고 그 합을 구하시오
+//		
+//		for (int i = 1; i <= 100; i++) {
+//			if (i%3 == 0 && i%5 == 0) {
+//				
+//			}
+//		}
+		
+		
+//		*
+//		**
+//		***
+//		****
+//		*****
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/com/baewooga/study/ch04/ForExample3.java
@@ -0,0 +1,25 @@
+package com.baewooga.study.ch04;
+
+public class ForExample3 {
+
+	public static void main(String[] args) {
+		
+//		*
+//		**
+//		***
+//		****
+//		*****
+		
+		for (int i = 0; i < 5; i++) {
+			
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+		
+	}
+
+}
