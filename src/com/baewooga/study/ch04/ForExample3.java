c1cd4f7d121f4d1f13fc5688d341c9a0f569ae70
minsoo822@naver.com <minsoo822@naver.com>
2022-08-19 11:08:09 +0900

    java

--- src/com/baewooga/study/ch04/ForExample3.java
+++ src/com/baewooga/study/ch04/ForExample3.java
@@ -12,10 +12,36 @@ public class ForExample3 {
 //		**********
 //		**********
 //		**********
-		
-		for (int i = 0; i < 10; i++) {
-			System.out.print("*");
+		for(int j = 0; j < 5; j++) {
+			for (int i = 0; i < 10; i++) {
+				System.out.print("*");
+			} System.out.println();
 		}
+		
+		System.out.println("----------------");
+		
+		
+//		11111
+//		22222
+//		33333
+//		44444
+//		55555
+		
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				System.out.print(i);
+			} System.out.println();
+		}
+		
+		System.out.println("----------------------");
+		
+//		12345
+//		12345
+//		12345
+//		12345
+		
+		
+		
 		
 		
 		
