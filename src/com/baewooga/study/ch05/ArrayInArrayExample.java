a99dd25c74cc723ea69901112c1d149b8fda9f20
minsoo822@naver.com <minsoo822@naver.com>
2022-08-19 17:13:28 +0900

    java

--- src/com/baewooga/study/ch05/ArrayInArrayExample.java
+++ src/com/baewooga/study/ch05/ArrayInArrayExample.java
@@ -100,6 +100,7 @@ public class ArrayInArrayExample {
 			}
 			
 			System.out.println("------------------------------");
+
 			//2 과목별 평균
 			for(int j=0; j<3; j++) {
 				int sum=0;
@@ -110,9 +111,17 @@ public class ArrayInArrayExample {
 			}
 					
 					
+			int[] sum2 = new int[3];
+			
+			for (int i = 0; i < scores.length; i++) {
+				for (int j = 0; j < scores[i].length; j++) {
+					sum2[j] += scores[i][j];
+				}
+			}
 					
-					
-					
+			for (int i = 0; i < sum2.length; i++) {
+				System.out.println(subject[i] + "과목의 평균점수는: " + ((double)sum2[i]/scores.length));
+			}		
 					
 					
 					
---
+++ src/com/baewooga/study/ch05/LottoPlease.java
@@ -0,0 +1,116 @@
+package com.baewooga.study.ch05;
+
+
+public class LottoPlease {
+
+	public static void main(String[] args) {
+		
+		
+//		로또번호 생성기
+//		현재까지 나온 번호랑 중복이 되지 않아야 함
+//		https://dhlottery.co.kr/gameResult.do?method=byWin
+//		여기에 현재까지 번호 다운 가능
+//		중복 숫자 가능
+//		3연속 번호 불가 123 456 789 등등
+//		보너스 번호는 제외
+//		
+//		Random rand = new Random();
+//		
+//		
+//		int[] numList = new int[6];
+//		
+//		for (int i=0; i<numList.length; i++) {
+//			numList[i] = rand.nextInt(45);
+//		}
+//		
+//		System.out.println(Arrays.toString(numList));
+////		
+		
+//		랜덤번호 배열 생성
+//		랜덤번호 추출 후 배열에 담기
+//		랜덤 번호 확인
+//		오름차순으로 순서 정렬
+		
+//		랜덤번호 생성
+		
+//		Random ran = new Random();
+//		int[] lotto = new int[6];
+//		
+//		for (int i = 0; i < lotto.length; i++) {
+//			lotto[i] = ran.nextInt(45);
+//		}
+//		System.out.println(Arrays.toString(lotto));
+//		
+//
+//		for (int i = 0; i < lotto.length; i++) {
+//			lotto[i] = ran.nextInt(45)+1;
+//			for (int j = 0; j < lotto.length; j++) {
+//				lotto[j] = ran.nextInt(45)+1;
+//				if (lotto[i] == lotto[j]) {
+//					i--;
+//					
+//				}
+//			}System.out.println(Arrays.toString(lotto));
+//		}
+		
+		
+//		배열
+//		랜덤번호 배열 생성
+//		
+//		Random random = new Random();
+//		
+//		int[] lotto = new int[6];		//
+//		
+//		
+//		for (int i = 0; i < lotto.length; i++) {
+//			lotto[i] = random.nextInt(45) +1;
+//			
+//		}
+//		System.out.println(Arrays.toString(lotto));
+		
+		
+//		랜덤한숫자를 배열에 집어넣기
+		
+		
+//		Random random = new Random();
+//		
+//		
+//		
+//		for (int i = 0; i < 6; i++) {
+//			System.out.print(random.nextInt(45));
+//		}
+//		
+//		System.out.println();
+//		System.out.println("---------------------");
+//		
+//		
+//		
+//		
+//		
+//		int[] lotto = new int[6];
+//		
+//		for (int i = 0; i < lotto.length; i++) {
+//			lotto[i] = random.nextInt(45) + 1 ;
+//		}
+//		System.out.println(Arrays.toString(lotto));
+//		
+		
+		
+//		랜덤숫자 생성
+		
+		
+//		(3연속)동일 중복숫자 제거
+		
+		
+//		이전 차수와 동일 제거
+		
+		double min = 1.0;
+		double max = 45.0;
+		
+		
+		
+
+
+	}
+
+}
